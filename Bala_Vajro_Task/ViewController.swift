79ee97ece0312d0638ca6efd89cd6cb40e86a383
krishbala905 <vanikrishnant12@gmail.com>
2022-12-23 20:20:36 +0530

    Initial Commit

---
+++ Bala_Vajro_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bala_Vajro_Task
+//
+//  Created by Mayandi Mariappan on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
